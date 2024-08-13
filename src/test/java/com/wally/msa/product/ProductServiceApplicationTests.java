14f2edb4e75aeb64c6abd23e347d28d15158ac0f
wally <goldenale5500@gmail.com>
2024-08-14 02:14:44 +0900

    테스트 코드 추가

--- src/test/java/com/wally/msa/product/ProductServiceApplicationTests.java
+++ src/test/java/com/wally/msa/product/ProductServiceApplicationTests.java
@@ -1,15 +1,57 @@
 package com.wally.msa.product;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
+
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.0.10");
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
+		String requestBody = """
+				{
+				  "name":  "product2",
+				  "description": "desc2",
+				  "price": 2000
+				}		    
+				""";
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/products")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo("product2"))
+				.body("description", Matchers.equalTo("desc2"))
+				.body("price", Matchers.equalTo(2000));
 	}
 
 }
